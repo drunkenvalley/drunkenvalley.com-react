e0f78498b6d7a991104bf752b0fbfd545ee11a74
Drunkenvalley <drunkenvalley@gmail.com>
2022-03-19 11:26:01 +0100

    Added baseurl to tsconfig, moved Firebase to folder

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,6 @@ import './App.css'
 
 import { Editor } from './Editor'
 
-import { FirebaseOptions, initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
-
-const firebaseConfig: FirebaseOptions = {
-  apiKey: 'AIzaSyAnSu4hqbs6jFAzFrlhVAUm-0FNu0Sqcqc',
-  authDomain: 'dulcet-palace-268221.firebaseapp.com',
-  projectId: 'dulcet-palace-268221',
-  storageBucket: 'dulcet-palace-268221.appspot.com',
-  messagingSenderId: '360712073555',
-  appId: '1:360712073555:web:5aa583f5d1618f917f71a6',
-  measurementId: 'G-L04WMBW2BP'
-}
-
-const firebase = initializeApp(firebaseConfig)
-const analytics = getAnalytics(firebase)
-
 function App () {
   return (
     <div className="App">
